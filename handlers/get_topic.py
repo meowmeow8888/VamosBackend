373ba3ae0eaa2bad37328463f6ba6eefd8a8e954
meowmeow8888 <mosseriguy8@gmail.com>
2026-07-22 22:10:55 +0300

    fix: added CORS origin

--- handlers/get_topic.py
+++ handlers/get_topic.py
@@ -1,5 +1,4 @@
 import hashlib
-from base64 import b64encode
 from os import environ
 
 
@@ -7,4 +6,4 @@ def user_id_to_topic(user_id):
     if user_id == -1:
         return "all"
     topic_pass = environ["TOPIC_PASS"]
-    return b64encode(hashlib.sha256(str(user_id).encode() + topic_pass.encode()).digest()).decode()
+    return hashlib.sha256(f"{user_id}{topic_pass}".encode()).hexdigest()
